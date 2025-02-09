280c0fcaada5875d56cf3a19780fe4d357f7edd0
João Manuel <2958530K@student.gla.ac.uk>
2025-02-09 16:21:33 +0000

    chapter 3 DONE - tests passed

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*k)pl0z(9i5-)jw#&*%wp_f7vyf9p6n@9utu*acj0wwl=46j##'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango',
 ]
 
 MIDDLEWARE = [
