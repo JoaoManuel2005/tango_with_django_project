e5b70cd538cc3512bbea9f31f799e72746d896f6
João Manuel <2958530K@student.gla.ac.uk>
2025-02-13 00:44:51 +0000

    chapter 10 DONE - tests passed

--- rango/views.py
+++ rango/views.py
@@ -7,27 +7,23 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.urls import reverse
+from datetime import datetime, timedelta
 
 def index(request):
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by the number of likes in descending order.
-    # Retrieve the top 5 only -- or all if less than 5.
-    # Place the list in our context_dict dictionary (with our boldmessage!)
-    # that will be passed to the template engine.
+    visitor_cookie_handler(request)
     category_list = Category.objects.order_by('-likes')[:5]
-    pages_list = Page.objects.order_by('-views')[:5]
-    context_dict = {}
-    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-    context_dict['categories'] = category_list
-    context_dict['pages'] = pages_list
-    # Render the response and send it back!
-    return render(request, 'rango/index.html', context=context_dict)
+    page_list = Page.objects.order_by('-views')[:5]
+    
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
+                    'categories': category_list,
+                    'pages': page_list}
+    
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-
-    # context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-
-    return render(request, 'rango/about.html' )#, context=context_dict)
+    visitor_cookie_handler(request)  # Ensure visit tracking is updated
+    context_dict = {'visits': request.session.get('visits', 1)}
+    return render(request, 'rango/about.html', context_dict)
 
 # def show_category(request, category_name_slug):
 #     # Create a context dictionary which we can pass
@@ -169,3 +165,24 @@ def user_logout(request):
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html')
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], "%Y-%m-%d %H:%M:%S")
+
+    # If more than a day has passed, increment the counter.
+    if (datetime.now() - last_visit_time).days > 0:
+        visits += 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
